831767c47b2834f205a5f048d3d508b33b3888ae
huutriCitynow <huutri.nguyen@m.citynow.vn>
2022-05-27 19:08:13 +0700

    Queue for Recommended Songs

--- src/app/music-player/components/queue/queue.component.ts
+++ src/app/music-player/components/queue/queue.component.ts
@@ -2,18 +2,20 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Song } from 'src/app/interfaces/song.interface';
 import { AudioPlayerService } from '../../services/audio-player.service';
 
-
 @Component({
   selector: 'app-queue',
   templateUrl: './queue.component.html',
-  styleUrls: ['./queue.component.scss']
+  styleUrls: ['./queue.component.scss'],
 })
 export class QueueComponent implements OnInit {
+  constructor(private audioService: AudioPlayerService) {}
+
   @Input() queue: Song[];
-  constructor() {
-  }
+  @Input() recommendedSongs: Song[];
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  updateRecommend() {
+    this.audioService.updateRecommend();
+  }
 }
