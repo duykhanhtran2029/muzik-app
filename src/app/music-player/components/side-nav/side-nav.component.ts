18eb86c434cbc63f37846b0d4b1e9ba578b84641
huutriCitynow <huutri.nguyen@m.citynow.vn>
2022-05-31 17:10:58 +0700

    Load Lyric when press Player in Side-nav

--- src/app/music-player/components/side-nav/side-nav.component.ts
+++ src/app/music-player/components/side-nav/side-nav.component.ts
@@ -1,32 +1,43 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { SearchComponent } from '../search/search.component';
-export type ROUTE = 'SEARCH' | 'HOME' | 'PLAYER' | 'ARTIST' | 'MANAGER' | 'PROFILE' | 'PLAYLIST';
+import { AudioPlayerService } from '../../services/audio-player.service';
+
+export type ROUTE =
+  | 'SEARCH'
+  | 'HOME'
+  | 'PLAYER'
+  | 'ARTIST'
+  | 'MANAGER'
+  | 'PROFILE'
+  | 'PLAYLIST';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
 })
-
 export class SideNavComponent implements OnInit {
-  constructor(private dialog: MatDialog,) { }
+  constructor(
+    private dialog: MatDialog,
+    public audioService: AudioPlayerService
+  ) {}
   currentRoute: ROUTE = 'HOME';
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   openSearchDialog() {
-    this.dialog.open(SearchComponent,
-      {
-        width: '800px',
-        height: '800px'
-        //data: { trigger: new ElementRef(event.currentTarget) }
-      });
-  }
-  
-  setRoute(route: ROUTE) {
-    this.currentRoute = route;
+    this.dialog.open(SearchComponent, {
+      width: '800px',
+      height: '800px',
+      //data: { trigger: new ElementRef(event.currentTarget) }
+    });
   }
 
+  setRoute(route: ROUTE) {
+    this.currentRoute = route;
+    if (route === 'PLAYER') {
+      this.audioService.loadLyric();
+    }
+  }
 }
