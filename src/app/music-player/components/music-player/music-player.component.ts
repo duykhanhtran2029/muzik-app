fa14bd01d3a7c994f701a01bd71b6b8853a2106d
huutriCitynow <huutri.nguyen@m.citynow.vn>
2022-05-31 17:11:32 +0700

    Lyric in Music-Player

--- src/app/music-player/components/music-player/music-player.component.ts
+++ src/app/music-player/components/music-player/music-player.component.ts
@@ -1,17 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 import { AudioPlayerService } from '../../services/audio-player.service';
+import { Lyric } from 'src/app/interfaces/lyric.interface';
+import { Song } from 'src/app/interfaces/song.interface';
+import { takeWhile } from 'rxjs';
 
 @Component({
   selector: 'app-music-player',
   templateUrl: './music-player.component.html',
-  styleUrls: ['./music-player.component.scss']
+  styleUrls: ['./music-player.component.scss'],
 })
-export class MusicPlayerComponent implements OnInit {
+export class MusicPlayerComponent implements OnInit, AfterViewInit {
+  constructor(
+    public audioService: AudioPlayerService,
+    private elem: ElementRef
+  ) {}
 
-  constructor(public audioService: AudioPlayerService) { }
+  lyric: Lyric[];
+  song: Song;
+  currentLyric: number;
+  componentActive = true;
+  playing: boolean;
 
-  lyric = 'Mùi hương hoa diên vĩ \n Hay là hương tóc mềm \n Ngàn vì sao chẳng sáng \n Hơn đôi mắt của em\nThật đẹp đến trăng thẹn thùng\nPhải nấp sau mây\nVậy thì cớ sao\nAnh đây lại nỡ buông tay\nỪ thì anh không muốn\nPhải nấp sau mây\nVậy thì cớ sao\nPhải nấp sau mây\nVậy thì cớ sao Mùi hương hoa diên vĩ Mùi hương hoa diên vĩ Mùi hương hoa diên vĩ Mùi hương hoa diên vĩ \n Hay là hương tóc mềm \n Ngàn vì sao chẳng sáng \n Hơn đôi mắt của em\nThật đẹp đến trăng thẹn thùng\nPhải nấp sau mây\nVậy thì cớ sao\nAnh đây lại nỡ buông tay\nỪ thì anh không muốn\nPhải nấp sau mây\nVậy thì cớ sao\nPhải nấp sau mây\nVậy thì cớ sao';
   ngOnInit(): void {
+    this.audioService
+      .getState()
+      .pipe(takeWhile(() => this.componentActive))
+      .subscribe((state) => {
+        this.song = state.song;
+        this.lyric = state.lyric;
+        this.currentLyric = state.currentLyric;
+        this.playing = state.playing;
+      });
   }
+  ngAfterViewInit(): void {
+    setInterval(() => {
+      if (this.currentLyric != -1 && this.playing) {
+        const currentActive = (<HTMLElement>(
+          this.elem.nativeElement
+        )).querySelector('.current');
 
+        currentActive.scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+        });
+      }
+    }, 500);
+  }
 }
