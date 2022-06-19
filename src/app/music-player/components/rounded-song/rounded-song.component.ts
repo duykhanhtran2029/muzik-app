a1f58cbd6260e6329cc05dc0c7234ccd166c6660
huutriCN <huutri.nguyen@m.citynow.vn>
2022-06-19 14:49:14 +0700

    fix: rounded Song CSS

--- src/app/music-player/components/rounded-song/rounded-song.component.ts
+++ src/app/music-player/components/rounded-song/rounded-song.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Song } from 'src/app/interfaces/song.interface';
+import { AudioPlayerService } from '../../services/audio-player.service';
 
 @Component({
   selector: 'app-rounded-song',
   templateUrl: './rounded-song.component.html',
-  styleUrls: ['./rounded-song.component.scss']
+  styleUrls: ['./rounded-song.component.scss'],
 })
 export class RoundedSongComponent implements OnInit {
   @Input() song: Song;
-  constructor() { }
+  constructor(public audioService: AudioPlayerService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  addToQueue() {
+    this.audioService.addToQueue(this.song);
   }
 
 }
