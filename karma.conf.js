08b013982c43cfed3b8771a1b24bdab9717b76d0
Kane Tran <kanetran29@gmail.com>
2022-07-14 21:14:50 +0700

    chore: change app name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ReplicaShazam-FE'),
+      dir: require('path').join(__dirname, './coverage/MUZIK-APP'),
       subdir: '.',
       reporters: [
         { type: 'html' },
